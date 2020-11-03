6ca5cfd3071e0eb577e55a927b0541e65f06b18c
Golden Ogbeka <ogbekagolden@gmail.com>
2020-11-03 18:53:50 +0100

    Initital Commit

---
+++ src/airlinereservationsystem/MainClass.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package airlinereservationsystem;
+
+/**
+ *
+ * @author Nedlog
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        Welcome obj=new Welcome();
+        obj.setSize(1000, 680);
+        obj.setVisible(true);
+        
+    }
+    
+}
